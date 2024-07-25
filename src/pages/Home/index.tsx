3544142683f24c40471016493a88369689b47d87
Jportov <joaovportog@gmail.com>
2024-07-25 15:48:11 -0300

    deploy
    novo deploy

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,86 +1,22 @@
-import { BannerCategoru } from '../../components/Banner'
-import { ProductResta } from '../../components/ProductList'
-import Restaurante from '../../models/Game'
-import sushi from '../../assets/images/image 1.png'
-import sushiA from '../../assets/images/imagem.png'
+import CardapioList from '../../components/Home/CardapioList'
+import Footer from '../../components/Home/Footer'
+import Hero from '../../components/Home/Hero'
 
-const Restaurantes: Restaurante[] = [
-  {
-    id: 1,
-    description:
-      'Peça já o melhor da culinária japonesa no conforto da sua casa! Sushis frescos, sashimis deliciosos e pratos quentes irresistíveis. Entrega rápida, embalagens cuidadosas e qualidade garantida.Experimente o Japão sem sair do lar com nosso delivery!',
-    title: 'Hioki sushi',
-    system: '4.9',
-    infos: ['Destaque da semana', 'Japonesa'],
-    image: sushiA,
-    imageN: '',
-    category: ''
-  },
-  {
-    id: 2,
-    category: '',
-    description:
-      'A La Dolce Vita Trattoria leva a autêntica cozinha italiana até você! Desfrute de massas caseiras, pizzas deliciosas e risotos incríveis, tudo no conforto do seu lar. Entrega rápida, pratos bem embalados e sabor inesquecível. Peça já!',
-    title: 'La Dolce Vita Trattoria',
-    system: '4.6',
-    infos: ['Italiana'],
-    image: sushi,
-    imageN: ''
-  },
-  {
-    id: 3,
-    category: '',
-    description:
-      'A La Dolce Vita Trattoria leva a autêntica cozinha italiana até você! Desfrute de massas caseiras, pizzas deliciosas e risotos incríveis, tudo no conforto do seu lar. Entrega rápida, pratos bem embalados e sabor inesquecível. Peça já!',
-    title: 'La Dolce Vita Trattoria',
-    system: '4.6',
-    infos: ['Italiana'],
-    image: sushi,
-    imageN: ''
-  },
-  {
-    id: 4,
-    category: '',
-    description:
-      'A La Dolce Vita Trattoria leva a autêntica cozinha italiana até você! Desfrute de massas caseiras, pizzas deliciosas e risotos incríveis, tudo no conforto do seu lar. Entrega rápida, pratos bem embalados e sabor inesquecível. Peça já!',
-    title: 'La Dolce Vita Trattoria',
-    system: '4.6',
-    infos: ['Italiana'],
-    image: sushi,
-    imageN: ''
-  },
-  {
-    id: 5,
-    category: '',
-    description:
-      'A La Dolce Vita Trattoria leva a autêntica cozinha italiana até você! Desfrute de massas caseiras, pizzas deliciosas e risotos incríveis, tudo no conforto do seu lar. Entrega rápida, pratos bem embalados e sabor inesquecível. Peça já!',
-    title: 'La Dolce Vita Trattoria',
-    system: '4.6',
-    infos: ['Italiana'],
-    image: sushi,
-    imageN: ''
-  },
-  {
-    id: 6,
-    category: '',
-    description:
-      'A La Dolce Vita Trattoria leva a autêntica cozinha italiana até você! Desfrute de massas caseiras, pizzas deliciosas e risotos incríveis, tudo no conforto do seu lar. Entrega rápida, pratos bem embalados e sabor inesquecível. Peça já!',
-    title: 'La Dolce Vita Trattoria',
-    system: '4.6',
-    infos: ['Italiana'],
-    image: sushi,
-    imageN: ''
+import { useGetFeatureRestaurantesQuery } from '../../services/api'
+
+const Home = () => {
+  const { data: home } = useGetFeatureRestaurantesQuery()
+
+  if (home) {
+    return (
+      <>
+        <Hero />
+        <CardapioList restaurante={home} />
+        <Footer />
+      </>
+    )
   }
-]
-const Home = () => (
-  <>
-    <BannerCategoru />
-    <ProductResta
-      title="Restaurantes"
-      background="black"
-      games={Restaurantes}
-    />
-  </>
-)
+  return <h3>Carregando...</h3>
+}
 
 export default Home
--- src/styles.ts
+++ src/styles.ts
@@ -1,49 +1,30 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const cores = {
-  branca: '#FFF8F2',
-  branca2: '#FFF8F3',
-  branca3: '#Fff',
-  preta: '#111',
-  verde: '#10AC84',
-  cinza: '#333',
-  cinzaClaro: '#a3a3a3',
-  rosa: '#e66767',
-  bege: '#FFEBD9'
+  amarelo: '#FFB930',
+  branca: '#FFFFFF',
+  fundoBody: '#FFF8F2',
+  trigo: '#FFEBD9',
+  salmao: '#E66767'
 }
 
 export const GlobalCss = createGlobalStyle`
-* {
-margin: 0;
-padding: 0;
-box-sizing: border-box;
-font-family: 'Roboto', sans-serif;
-list-style:none;
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: 'Roboto', sans-serif;
+    list-style: none;
+  }
 
-}
-body {
-  background-color: ${cores.preta};
-  color: ${cores.branca};
+  body {
+    background-color: ${cores.fundoBody};
+  }
 
-}
   .container {
     max-width: 1024px;
     width: 100%;
     margin: 0 auto;
-    }
-
-  @media screen and (max-width: 768px) {
-    body {
-      width:100%;
-      max-width:100%;
-      display: grid;
-      grid-template-columns: 1fr;
-      background-color: ${cores.branca};
-    }
-
-    .container {
-      max-width: 100%;
-      padding: 0 20px;
-    }
   }
+
 `
